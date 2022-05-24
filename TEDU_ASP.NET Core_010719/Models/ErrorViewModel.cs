f5aec1c4b2ef0c28434b5a4278267aa829a83a9e
Cris Dev <vva1994@gmail.com>
2022-05-24 23:46:26 +0700

    Add project files.

---
+++ TEDU_ASP.NET Core_010719/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TEDU_ASP.NET_Core_010719.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
